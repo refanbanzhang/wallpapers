701fb9ba07d3d41e98194c6ea758e133fe17fd42
refanbanzhang <gaoqiang19514@163.com>
2025-05-15 07:25:38 +0800

    feat: 创建首页

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,111 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+import img1 from '@/assets/1.jpg'
+import img2 from '@/assets/2.jpg'
+
+interface Wallpaper {
+  id: string
+  urls: {
+    small: string
+  }
+  alt_description: string
+}
+
+const wallpapers = ref<Wallpaper[]>([
+  {
+    id: '1',
+    urls: {
+      small: img1,
+    },
+    alt_description: 'Wallpaper 1',
+  },
+  {
+    id: '2',
+    urls: {
+      small: img2,
+    },
+    alt_description: 'Wallpaper 2',
+  },
+  {
+    id: '3',
+    urls: {
+      small: img1,
+    },
+    alt_description: 'Wallpaper 2',  
+  },
+  {
+    id: '4',
+    urls: {
+      small: img2,
+    },
+    alt_description: 'Wallpaper 2',
+  },
+  {
+    id: '5',
+    urls: {
+      small: img1,
+    },
+    alt_description: 'Wallpaper 2',
+  },
+  {
+    id: '6',
+    urls: {
+      small: img2,
+    },
+    alt_description: 'Wallpaper 2',
+  },
+])
+
+const onOpenModal = (wallpaper: Wallpaper) => {
+  console.log(wallpaper)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="container">
+    <div class="header">
+      <h1>Wallpapers</h1>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="wallpapers">
+      <div
+        class="wallpaper"
+        v-for="wallpaper in wallpapers"
+        :key="wallpaper.id"
+        @click="onOpenModal(wallpaper)"
+      >
+        <img :src="wallpaper.urls.small" :alt="wallpaper.alt_description" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.container {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  height: 100%;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.header {
+  padding: 10px;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.wallpapers {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.wallpaper {
+  width: 20%;
+  padding: 5px;
+  border-radius: 5px;
+  overflow: hidden;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
